9f03a02a38a8762ae88bd3035bdc77b21784e829
Zephri <dingbats2@hotmail.co.uk>
2018-12-23 22:56:35 +0000

    Updated "Build terminal".

---
+++ Addons/Build terminal/dv_terminal/cl_init.lua
@@ -0,0 +1,12 @@
+include("shared.lua")
+
+function ENT:Draw()
+	local ply = LocalPlayer()
+	self:DrawModel()
+
+	if ply:GetPos():Distance(self:GetPos()) then
+		self.CanDraw = true
+	else
+		self.CanDraw = false
+	end
+end
---
+++ Addons/Build terminal/dv_terminal/init.lua
@@ -0,0 +1,30 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Initialize()
+
+	-- Dealing with the model.
+	elseif !self.vip then self:SetModel("models/props_combine/combine_intmonitor001.mdl")
+	elseif self.vip then self:SetModel("models/props_combine/combine_monitorbay.mdl")
+	else self:SetModel("models/props_combine/combine_interface001.mdl") end
+
+	-- Setting up the normal ent here.
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+
+	-- Build the spawn area here
+end
+
+function ENT:OnRemove()
+	-- Remove spawn area here
+end
+
+function ENT:Think()
+	-- Check spawn area clear here
+end
+
+function ENT:Use(a,c)
+	-- Open the gui for the player here
+end
