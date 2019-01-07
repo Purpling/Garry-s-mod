50335c5be8b73bddd384c908b14771d45eccf1b4
Zephri <dingbats2@hotmail.co.uk>
2019-01-07 06:37:39 +0000

    Update to make running optional.

--- hashsystem/_hashsystemtesting.lua
+++ hashsystem/_hashsystemtesting.lua
@@ -1,14 +1,18 @@
-hook.Add("hashsystemloaded","Waitingforhashsystem",function()
-    
-    hashsystem.handle["testfile"] = function(file,data) print(data) end
+-- Something if you want to see how the system works.
+-- Change the false into a true to use.
+if false then
+    hook.Add("hashsystemloaded","Waitingforhashsystem",function()
+        
+        hashsystem.handle["testfile"] = function(file,data) print(data) end
 
-    if CLIENT then return end
+        if CLIENT then return end
 
-    local hash,error = hashsystem.generate("testfile")
-    print(hash)
-    print(error)
+        local hash,error = hashsystem.generate("testfile")
+        print(hash)
+        print(error)
 
-    if error==nil then
-        hashsystem.relay(hash,"testfile","testfile","works")
-    end
-end)
+        if error==nil then
+            hashsystem.relay(hash,"testfile","testfile","works")
+        end
+    end)
+end
